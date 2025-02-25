37474c4eb32cfe0a9caa31867b0a311c8b9a45cc
KaushikiDhupar <145760175+KaushikiDhupar@users.noreply.github.com>
2025-02-26 00:22:45 +0530

    Time: 790 ms (15.09%) | Memory: 143.5 MB (6.16%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.py
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.py
@@ -1,24 +1,22 @@
 from collections import deque
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
-        q=deque([source])
+
         adj_list = {i: [] for i in range(n)}
         visited=set([source])
         for u,v in edges:
             adj_list[u].append(v)
             adj_list[v].append(u)
 
-
-        while q:
-            node=q.popleft()
-            if node==destination :
+        def dfs(node):
+            if node==destination:
                 return True
-            for neighbours in adj_list[node]:
-                if neighbours not in visited:
-                    q.append(neighbours)
-                    visited.add(neighbours)
-            
-        return False
+            visited.add(node)
+            for neighbour in adj_list[node]:
+                if neighbour not in visited and dfs(neighbour):
+                    return True
+            return False
+        return dfs(source)
 
             
 
