9289130ef605ee771c499eb4b1f1f670003ca418
KaushikiDhupar <145760175+KaushikiDhupar@users.noreply.github.com>
2025-03-19 13:58:42 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (82.65%) - LeetSync

---
+++ 198-house-robber/house-robber.py
@@ -0,0 +1,25 @@
+
+class Solution:  
+    def f(self,index, nums,dp):
+        if(index==0):
+            return nums[index]
+        if(index<0):
+            return 0
+        if(dp[index]!=-1):
+            return dp[index]
+        rob=nums[index]+self.f(index-2,nums,dp)
+        notrob=0+self.f(index-1,nums,dp)
+
+        dp[index]=max(rob,notrob)
+
+        return dp[index]
+
+    def rob(self, nums: List[int]) -> int:
+        n=len(nums)
+        dp=[-1]*n
+        return self.f(n-1,nums,dp)
+
+        
+
+
+        
