c4af21bc4ecc09f007af2c67cfad03f5d999de42
KaushikiDhupar <145760175+KaushikiDhupar@users.noreply.github.com>
2025-04-03 00:02:22 +0530

    Time: 1 ms (32.20%) | Memory: 17.7 MB (65.51%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -1,23 +1,22 @@
 class Solution:
-    def countways(self,i,j,dp):
-        # Base case: If we reach the top-left corner (i=0, j=0), there is one way to reach there.
-        if i==0 and j==0:
-            return 1
-
-        if i<0 or j<0: # If either i or j goes out of bounds (negative), there is no way to reach that cell.
-            return 0
-
-        # If we have already calculated the number of ways for this cell, return it from the dp array.
-        if dp[i][j]!=-1:
-            return dp[i][j]
-
-        # Recursive calls to count the number of ways to reach the current cell.
-        up=self.countways(i-1,j,dp) #moving up one row
-        left=self.countways(i,j-1,dp) #moving left
-        dp[i][j]=left+up
-        return dp[i][j]
-
     def uniquePaths(self, m: int, n: int) -> int:
-        dp=[[-1 for _ in range(n)] for _ in range(m)]
-        return self.countways(m-1,n-1,dp)
+        #initialize a dp array
+        dp=[[0 for _ in range(n)] for _ in range(m)]
+        dp[0][0]=1
+        for i in range(m):
+            for j in range(n):
+                 # Base condition: If we are at the top-left corner, there is one way to reach it.
+                if i == 0 and j == 0:
+                    dp[i][j] = 1
+                    continue
+                
+                # Initialize variables to store the number of ways from above and from the left.
+                up = 0
+                left = 0
+                if i>0:
+                    up=dp[i-1][j]
+                if j>0:
+                    left=dp[i][j-1]
+                dp[i][j]=up+left
+        return dp[m-1][n-1]
         
