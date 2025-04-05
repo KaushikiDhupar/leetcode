fdbba41c09b422348edd68b287ca459651a12cda
KaushikiDhupar <145760175+KaushikiDhupar@users.noreply.github.com>
2025-04-05 16:36:45 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (29.68%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.py
+++ 63-unique-paths-ii/unique-paths-ii.py
@@ -1,25 +1,30 @@
 class Solution:
-    def solve(self,dp,i,j,obstacleGrid):
-        if i>=0 and j>=0 and obstacleGrid[i][j]==1:
-            return 0
-        if i<0 or j<0:
-            return 0
-        if i==0 and j==0:
-            return 1
-        if dp[i][j]!=-1:
-            return dp[i][j]
-        up=self.solve(dp,i-1,j,obstacleGrid)
-        left=self.solve(dp,i,j-1,obstacleGrid)
-        dp[i][j]=up+left
-        return dp[i][j]
-    
-    
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
-        m=len(obstacleGrid)
-        n=len(obstacleGrid[0])
-        dp=[[-1 for j in range(n)] for i in range(m)]
-        return self.solve(dp,m-1,n-1,obstacleGrid)
-        
-        
+        rows,cols=len(obstacleGrid),len(obstacleGrid[0])
+        dp=[[-1 for _ in range(cols)] for _ in range(rows)]
+        if obstacleGrid[0][0] == 1:
+            return 0
+        dp[0][0] = 1
+
+        for i in range(rows):
+            for j in range(cols):
+                if obstacleGrid[i][j]==1:
+                    dp[i][j]=0
+                    continue
+                if i==0 and j==0:
+                    continue #already initialized
+              
+                up=0
+                left=0
+                
+                if i>0:
+                    up=dp[i-1][j]
+                if j>0:
+                    left=dp[i][j-1]
+                dp[i][j]=up+left
+
+        return dp[rows-1][cols-1]
+
+
 
         
