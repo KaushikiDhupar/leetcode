2bbd6c184142b2ba8742a5061a695a9c132c23d4
KaushikiDhupar <145760175+KaushikiDhupar@users.noreply.github.com>
2025-04-06 18:26:47 +0530

    Time: 19 ms (36.46%) | Memory: 20.2 MB (50.01%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.py
+++ 64-minimum-path-sum/minimum-path-sum.py
@@ -1,26 +1,14 @@
 class Solution:
-    def solve(self,dp,i,j,grid):
-        if i==0 and j==0:
-            return grid[0][0]
-        if i<0 or j<0:
-            return float('inf')
-        if dp[i][j]!=-1:
-            return dp[i][j]
-        up=grid[i][j]+self.solve(dp,i-1,j,grid)
-        left=grid[i][j]+self.solve(dp,i,j-1,grid)
-        dp[i][j]=min(up,left)
-        return dp[i][j]
-
-
-        
-
     def minPathSum(self, grid: List[List[int]]) -> int:
         rows,cols=len(grid),len(grid[0])
-        dp=[[-1 for _ in range(cols)] for _ in range(rows)]
-        return self.solve(dp,rows-1,cols-1,grid)
-
-
-        
-        
-
+        dp=[[-1 for _ in range(cols)] for j in range(rows)]
+        for i in range(rows):
+            for j in range(cols):
+                if i==0 and j==0:
+                    dp[i][j]=grid[i][j]
+                else:
+                    up = grid[i][j] + (dp[i-1][j] if i > 0 else float('inf'))
+                    left = grid[i][j] + (dp[i][j-1] if j > 0 else float('inf'))
+                    dp[i][j]=min(up,left)
+        return dp[rows-1][cols-1]
         
