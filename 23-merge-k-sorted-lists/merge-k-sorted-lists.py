5d1c1728eb1b845a57cfb6ed8dbcd9438f0899c9
KaushikiDhupar <145760175+KaushikiDhupar@users.noreply.github.com>
2025-06-22 19:37:03 +0530

    Time: 15 ms (38.07%) | Memory: 21.2 MB (5.66%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.py
@@ -1,29 +1,32 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
-    def mergeKLists(self, lists: List[ListNode]) -> ListNode:
-        if not lists:
-            return None
-        if len(lists) == 1:
-            return lists[0]
-        
-        mid = len(lists) // 2
-        left = self.mergeKLists(lists[:mid])
-        right = self.mergeKLists(lists[mid:])
-        
-        return self.merge(left, right)
-    
-    def merge(self, l1, l2):
-        dummy = ListNode(0)
-        curr = dummy
-        
-        while l1 and l2:
-            if l1.val < l2.val:
-                curr.next = l1
-                l1 = l1.next
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        def merge2list(l1,l2):
+            if l1==None: return l2
+            elif l2==None: return l1
             else:
-                curr.next = l2
-                l2 = l2.next
-            curr = curr.next
+                if l1.val<=l2.val:
+                    l1.next=merge2list(l1.next,l2) 
+                    return l1
+                else:
+                    l2.next=merge2list(l1,l2.next)
+                    return l2
+        def PartitionandMerge(startnode,endnode,lists):
+            if startnode==endnode:
+                return lists[startnode]
+            else:
+                mid=startnode+(endnode-startnode)//2
+                l1=PartitionandMerge(startnode,mid,lists)
+                l2=PartitionandMerge(mid+1,endnode,lists)
+                return merge2list(l1,l2)
+
+        if not lists or len(lists) == 0:
+            return None
+
+        return PartitionandMerge(0, len(lists) - 1, lists)
+
         
-        curr.next = l1 or l2
-        
-        return dummy.next
