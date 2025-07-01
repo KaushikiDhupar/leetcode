ac373456fcae7d44f9415b290d337107448889c5
KaushikiDhupar <145760175+KaushikiDhupar@users.noreply.github.com>
2025-07-01 23:32:13 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (37.67%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.py
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.py
@@ -7,19 +7,21 @@
 from collections import deque
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
-        res=[]
         q=deque([root])
+        res=[]
 
         while q:
-            rightSide=None # Tracks the last node encountered at the current level
+            rightside=None
+            qlen=len(q)
+            for _ in range(qlen):
+                node=q.popleft()
             
-            qlen=len(q)# Number of nodes in the current level
-            for i in range(qlen):
-                node=q.popleft() # Dequeue the next node in the current level
                 if node:
-                    rightSide=node # Update `rightSide` to the current node
-                    q.append(node.left)# Add the left child to the queue for the next level
-                    q.append(node.right)# Add the right child to the queue for the next level
-            if rightSide:
-                res.append(rightSide.val)# Add the value of the rightmost node to the result list
+                    rightside=node
+                    if node.left:
+                        q.append(node.left)
+                    if node.right:
+                        q.append(node.right)
+            if rightside:
+                res.append(rightside.val)
         return res
