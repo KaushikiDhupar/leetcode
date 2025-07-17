0e5c7badac082c84f19f145eabbd8f70c318e579
KaushikiDhupar <145760175+KaushikiDhupar@users.noreply.github.com>
2025-07-17 21:44:33 +0530

    Time: 0 ms (100.00%) | Memory: 23.5 MB (10.20%) - LeetSync

---
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.py
@@ -0,0 +1,31 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def countNodes(self, root: Optional[TreeNode]) -> int:
+        def getLeftNodes(root): #calculate height of left subtree
+            temp=root
+            lh=0
+            while temp:
+                temp=temp.left
+                lh+=1
+            return lh
+        def getRightNodes(root): #calculate height of right subtree
+            temp=root
+            rh=0
+            while temp:
+                temp=temp.right
+                rh+=1
+            return rh
+        if not root:
+            return 0
+        lh=getLeftNodes(root)
+        rh=getRightNodes(root)
+
+        if lh==rh: # i.e the subtree is a perfect binary tree
+            return (2**lh)-1
+        return self.countNodes(root.left)+self.countNodes(root.right)+1
+        
