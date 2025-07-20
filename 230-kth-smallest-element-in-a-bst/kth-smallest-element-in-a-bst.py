cde61da9fc9ea055c6820f73902aed9f3f11abb0
KaushikiDhupar <145760175+KaushikiDhupar@users.noreply.github.com>
2025-07-20 22:35:16 +0530

    Time: 7 ms (5.70%) | Memory: 21.2 MB (27.17%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.py
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.py
@@ -6,6 +6,11 @@
 #         self.right = right
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        """
+        inorder traversal and increment the count(by decreasing k value) when backtracking up
+        from the leftmost node when k==0 we have the value of kth smallest
+        O(log(n) + k) space: O(n): recursion stack
+        """
         self.k=k
         self.ans=-1
         def inorder(node):
